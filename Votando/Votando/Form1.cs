13df7909e8ec0940975b70b12b1916ea21eaf98b
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-04-21 12:48:00 -0300

    Exercicio Votando, capitulo 3

---
+++ Votando/Votando/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Votando
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 16;
+            bool brasileira = false;
+            bool podeVotar = (idade >= 16) && (brasileira == true);
+
+            if(podeVotar)
+            {
+                MessageBox.Show("Você está apto (a) a votar");
+            }
+            else
+            {
+                MessageBox.Show("Você não está apto (a) a votar");
+            }
+        }
+    }
+}
