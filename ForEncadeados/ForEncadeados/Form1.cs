03db61d8a3a1705b7d8609448adce7c75fa5ddfd
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-04-22 23:19:34 -0300

    Exercicio de For encadeados

---
+++ ForEncadeados/ForEncadeados/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEncadeados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int qtdeDeLinhas = 5;
+            String triangulo = "";
+            for(int i = 1; i <= qtdeDeLinhas; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    triangulo += j * i + " ";
+                }
+                triangulo += "\n";
+            }
+            MessageBox.Show(triangulo);
+        }
+    }
+}
