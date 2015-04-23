136e06a92b4fe71e25969ad3c5be4178fbad11e1
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-04-23 15:58:16 -0300

    Exercicio com Classe, atributos e metodos

---
+++ ExemploClasseConta/ExemploClasseConta/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploClasseConta
+{
+    class Conta
+    {
+        public double saldo;
+        public string titular;
+        public int numero;
+        public string cpf;
+        public int agencia;
+
+        public void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+
+        public void Deposita(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
---
+++ ExemploClasseConta/ExemploClasseConta/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploClasseConta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            conta.numero = 00001;
+            conta.saldo = 5000.0;
+            conta.titular = "Marina Costa Silva";
+
+            MessageBox.Show("Nome do titular é: " + conta.titular);
+
+            Conta contaDois = new Conta();
+            contaDois.numero = 00002;
+            contaDois.saldo = 400.0;
+            contaDois.titular = "José Carlos";
+            contaDois.agencia = 0909;
+            contaDois.cpf = "09009009080";
+
+            MessageBox.Show("Segunda Conta ---- Titular: " + contaDois.titular + " Saldo: " + contaDois.saldo
+                + " Número: " + contaDois.numero + " Agência: " + contaDois.agencia);
+        }
+    }
+}
