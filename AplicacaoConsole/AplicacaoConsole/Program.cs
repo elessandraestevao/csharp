978bc9a34cea26bc831500bcc541bfbd707e8126
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-05-04 00:03:15 -0300

    Manipulando dados do console

---
+++ AplicacaoConsole/AplicacaoConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite suas mensagens! Para sair digite CTRL + Z");
+            TextReader leitor = Console.In;
+            string linha = leitor.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = leitor.ReadLine();
+            }
+        }
+    }
+}
